f269f5747c81f1dd891815b0097ef0c82a6848fb
Mohd Miyan Khan <mohdmiyankhan005@gmail.com>
2025-01-17 23:53:56 +0530

    Add project files.

---
+++ SSMS.API/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using SSMS.Application;
+using SSMS.Infrastructure;
+
+namespace SSMS.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddSSMSDI(this IServiceCollection service)
+        {
+            service.AddApplicationDI()
+                .AddInfrastructureDI();
+            return service;
+        }
+    }
+}
